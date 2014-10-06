2fac517c6100f7a9831dca851a7e609cb5bda245
kkroep <k.kroep1@gmail.com>
2014-10-06 14:43:16 +0200

    First version
    Version with single player on sphere

---
+++ Z3D_Kees_01/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public float cam_speed;
+	public float cam_rot_speed;
+	
+	public float smoothTime = 0.3F;
+    private float Velocity = 10.0F;
+	
+	public GameObject Cam_Attachement;
+	public Player_Physics_Controller player_Physics_Controller;
+	
+	private Vector3 new_Position;
+	// Use this for initialization
+	void Start () {
+	new_Position = Cam_Attachement.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		new_Position = Cam_Attachement.transform.position;
+		
+		//new_Position.x = Mathf.SmoothDamp(transform.position.x, Cam_Attachement.transform.position.x, ref Velocity, smoothTime);
+		//new_Position.y = Mathf.SmoothDamp(transform.position.y, Cam_Attachement.transform.position.y, ref Velocity, smoothTime);
+		//new_Position.z = Mathf.SmoothDamp(transform.position.z, Cam_Attachement.transform.position.z, ref Velocity, smoothTime);
+		
+		
+		transform.position = new_Position;
+		//transform.rotation = Cam_Attachement.transform.rotation;
+		//rigidbody.velocity =(Cam_Attachement.transform.position-transform.position)*cam_speed;
+		transform.LookAt(player_Physics_Controller.transform, player_Physics_Controller.current_normal);
+		//transform.rotation = Quaternion.LookRotation(player_Physics_Controller.rigidbody.velocity,player_Physics_Controller.current_normal);
+	}
+}
