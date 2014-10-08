751397447a78c7cec44e9a635a6635fd9b32520c
jcalixtofiguei <joao@wlan-145-94-253-053.wlan.tudelft.nl>
2014-10-08 10:20:41 +0200

    Re-Added Networking Test (organized)

---
+++ Networking_Test/Assets/FreeMovementScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreeMovementScript : MonoBehaviour {
+	public float speed;
+
+	void FixedUpdate(){
+		if (networkView.isMine) {
+			float horizontalComponent = Input.GetAxis ("Horizontal");
+			float verticalComponent = Input.GetAxis ("Vertical");
+			Vector3 movement = new Vector3 (horizontalComponent, 0.0f, verticalComponent);
+			rigidbody.AddForce (movement * speed * Time.deltaTime);
+		}
+		else{
+			enabled = false;
+		}
+	}
+}
