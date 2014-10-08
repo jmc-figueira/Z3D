c62415236530b872b6b1e4b95153bc8b40b6af81
jcalixtofiguei <joao@wlan-145-94-253-053.wlan.tudelft.nl>
2014-10-08 09:55:43 +0200

    Added Networking Test Project

---
+++ Assets/RandomColorGeneration.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomColorGeneration : MonoBehaviour {
+	
+	void Start () {
+		renderer.material.color = new Color (Random.value, Random.value, Random.value);
+	}
+}
