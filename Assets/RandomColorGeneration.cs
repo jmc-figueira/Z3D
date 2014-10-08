2e3040ce97ad8a19a9fbc64fe2085fb170064158
jcalixtofiguei <joao@wlan-145-94-253-053.wlan.tudelft.nl>
2014-10-08 10:12:22 +0200

    Reverting to previous commit

--- Assets/RandomColorGeneration.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class RandomColorGeneration : MonoBehaviour {
-	
-	void Start () {
-		renderer.material.color = new Color (Random.value, Random.value, Random.value);
-	}
-}
