26f75e5821c1720e9bdeedd57e79dac264835e84
Mkooper <martin.kooper@gmail.com>
2014-10-15 12:41:05 +0200

    ik ben martin

---
+++ Z3D_Kees_01/Assets/_Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+    public Transform target;
+    void Update() {
+		
+		Vector3 relativepos = (target.position - transform.position);
+        transform.rotation = Quaternion.LookRotation(relativepos, new Vector3(0,0,1));
+		transform.Rotate(90f,0f,0f);
+		
+		
+    }
+}
---
+++ Z3D_Kees_01/Assets/_Scripts/orbits.cs
@@ -0,0 +1,31 @@
+﻿    using UnityEngine;
+    using System.Collections;
+     
+    public class orbits : MonoBehaviour {
+     
+    public GameObject cent;
+	public float rotationSpeed;
+	
+	
+    private Transform center;
+    private Vector3 axis;
+	
+    
+    void Start () {
+	
+	
+    center = cent.transform;
+	
+	//random orbit rotation
+    axis = Random.insideUnitSphere;
+	
+    }
+     
+    void Update () {  //apperantly works smoother when using FixedUpdate
+	transform.RotateAround (center.position, axis, rotationSpeed * Time.deltaTime);
+    }
+	
+    }
+	
+	
+	
