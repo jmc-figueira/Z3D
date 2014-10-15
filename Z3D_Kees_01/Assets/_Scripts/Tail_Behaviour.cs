02a1ca53f63a94fe711fabbc119e0366a44d3b7e
kkroep <k.kroep1@gmail.com>
2014-10-15 11:25:14 +0200

    small

---
+++ Z3D_Kees_01/Assets/_Scripts/Tail_Behaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tail_Behaviour : MonoBehaviour {
+
+	public float Tail_Timer;
+	private float timer;
+	// Use this for initialization
+	void Start () {
+		timer = Tail_Timer;
+		collider.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer>0){
+		}else{
+		timer=0f;
+		}
+	}
+}
